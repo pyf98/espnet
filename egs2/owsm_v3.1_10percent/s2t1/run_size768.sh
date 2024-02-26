1599283e22636f0bfb9d332c5eedb27d086b0f36
Yifan Peng <pengyf21@gmail.com>
2024-02-25 18:00:28 -0600

    add 10percent

---
+++ egs2/owsm_v3.1_10percent/s2t1/run_size768.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+train_set=train
+valid_set=dev
+# test_sets="test/LibriSpeech/test_clean test/LibriSpeech/test_other test/SWBD/eval2000 test/TEDLIUM/test"
+test_sets=dev
+
+nbpe=50000
+s2t_config=conf/train_s2t_ebf_conv2d_size768_e9_d9_piecewise_lr5e-4_warmup60k_flashattn.yaml
+inference_config=conf/decode_s2t.yaml
+
+# inference only args
+# --cleaner whisper_en --hyp_cleaner whisper_en
+./s2t.sh \
+    --stage 11 \
+    --stop_stage 11 \
+    --use_lm false \
+    --num_nodes 4 \
+    --ngpu 4 \
+    --nj 64 \
+    --gpu_inference true \
+    --inference_nj 64 \
+    --num_splits_s2t 1 \
+    --feats_type raw \
+    --audio_format flac.ark \
+    --token_type bpe \
+    --nbpe ${nbpe} \
+    --bpe_input_sentence_size 15000000 \
+    --s2t_config "${s2t_config}" \
+    --inference_config "${inference_config}" \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --bpe_train_text "dump/raw/${train_set}/text" \
+    --bpe_nlsyms data/nlsyms.txt \
+    --lm_train_text "dump/raw/${train_set}/text" "$@"
